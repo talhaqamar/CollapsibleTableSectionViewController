c1408be5710b9dc5ee22e6438356751ab12dfbbe
Talha Qamar <talha.kicsit18@gmail.com>
2018-10-27 16:10:28 +1100

    Created new sample project using the pod

---
+++ CustomExample/CustomCollapsibleTableSectionViewController/CustomCollapsibleTableSectionViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCollapsibleTableSectionViewController
+//
+//  Created by Talha Qamar on 27/10/18.
+//  Copyright © 2018 devtalha.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CustomExample/CustomCollapsibleTableSectionViewController/Pods/Target Support Files/Pods-CustomCollapsibleTableSectionViewController/Pods-CustomCollapsibleTableSectionViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomCollapsibleTableSectionViewController : NSObject
+@end
+@implementation PodsDummy_Pods_CustomCollapsibleTableSectionViewController
+@end
